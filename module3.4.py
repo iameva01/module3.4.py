4ee5e538e70f1980588eac68f8f059ff56eb747e
iameva01 <evatagirova@gmail.com>
2024-08-03 15:20:19 +0300

    Update module3.4.py

--- module3.4.py
+++ module3.4.py
@@ -1,10 +1,16 @@
-def single_root_words(root_word = 'дерево', *other_words):
+def single_root_words(root_word, *other_words):
+    root_word_lower = root_word.lower()
     same_words = []
+
     for word in other_words:
-        if root_word in word or word in root_word:
+        word_lower = word.lower()
+        if root_word_lower in word_lower or word_lower in root_word_lower:
             same_words.append(word)
+    
     return same_words
+
+# Примеры вызова функции и вывод результатов
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+print(result1)  # ['richiest', 'orichalcum', 'richies']
+print(result2)  # ['Able', 'Disable']
